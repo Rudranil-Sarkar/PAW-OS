73a9a12980a8b8c606ba558278f43805188ed849
Rudranil Sarkar <rudronil34@gmail.com>
2021-06-15 00:10:52 +0530

    Changed both files to support the new structure

--- kernel/idt.c
+++ kernel/idt.c
@@ -1,30 +1,12 @@
 #include "idt.h"
 #include "font_renderer.h"
+#include "pic.h"
+#include "isr.h"
 
 __attribute__((aligned(0x10)))
 static idt_table_entry idt_table[MAX_ENTRIES];
 
 static idt_info idt;
-
-/* A generic exception handler that just completely hanges the computer */
-__attribute__((noreturn))
-void exception_handler(void);
-void exception_handler() { print_string("error", 5, 0, 0, 0x5B); __asm__("cli\n\thlt"); while(1) {} }
-
-/* So this is the way we have to define interrupt and exception routines in gcc */
-struct interrupt_frame {
-    u32 ip;
-    u32 cs;
-    u32 flags;
-    u32 sp;
-    u32 ss;
-} __attribute__((packed));
-
-__attribute__((interrupt))
-void int20 (struct interrupt_frame * frame)
-{
-    print_string("Testing Interrupt", 17, 0, 100, 0x5B);
-}
 
 void add_entry(u8 vector, void (*isr)(), u8 flags)
 {
@@ -42,11 +24,13 @@ void idt_init(void)
     idt.base = (u32) &idt_table[0];
     /* The first 32 entries are reserved by the cpu used for exceptions */
      for (u8 vector = 0; vector < 32; vector++)
-        add_entry(vector, exception_handler, 0x8E);
+        add_entry(vector, exception_handler, 0x8F);
 
-     add_entry(0x20, int20, 0x8E);
+     add_entry(0x20, irq0, 0x8E);
+     add_entry(0x21, irq1, 0x8E);
+
     __asm__ volatile ("lidt %0": : "memory"(idt));
-    __asm__ volatile ("movb $0xff, %al; outb %al, $0xa1; outb %al, $0x21");         // Disabling the PIC chip
+    disable_pic();
     __asm__ volatile ("sti");
 }
 
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,18 +2,13 @@
 #include "paw_bitmap.h"
 #include "font_renderer.h"
 #include "idt.h"
-
-int strlen(char* str)
-{
-	int res = 0;
-	for (int i = 0; str[i] != '\0'; i++)
-		res++;
-	return res;
-}
+#include "pic.h"
 
 void _main()
 {
 	idt_init();
+	init_pic(0x20, 0x28);
+
 	u8 startx = 110;
 	u8 starty = 30;
 	for (int i = 0; i < 100; i++)
@@ -24,6 +19,5 @@ void _main()
 		}
 	}
 	char* paw_os = "PAW OS";
-	print_string(paw_os, strlen(paw_os), 138, 120, 0x5B);
-	__asm__ volatile ("int $0x20");
+	print_string_exact(paw_os, strlen(paw_os), 138, 120, 0x5B);
 }
