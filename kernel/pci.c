515df79a9018184c50d8bd6abf147e778c839ea5
Rudranil Sarkar <rudronil34@gmail.com>
2021-12-21 01:09:29 +0530

    updated the .gitignore file

---
+++ kernel/pci.c
@@ -0,0 +1,2 @@
+#include "pci.h"
+
---
+++ kernel/pci.h
@@ -0,0 +1,10 @@
+#ifndef __PCI_H__
+#define __PCI_H__
+
+#include "typedef.h"
+
+#define PCI_CONFIG_ADDRESS 0xCF8
+#define PCI_CONFIG_DATA    0xCFC
+
+
+#endif // __PCI_H__
