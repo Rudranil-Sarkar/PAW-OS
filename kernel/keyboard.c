38609264c4a2309e85ac97994797d79415b5c58a
Rudranil Sarkar <rudronil34@gmail.com>
2021-12-25 19:35:50 +0530

    Fixing my drunken state shitty programming

--- kernel/keyboard.c
+++ kernel/keyboard.c
@@ -12,10 +12,19 @@ char decode_scancode(u8 scancode)
      /* but this will do for now I will fix this later                                             */
      /**********************************************************************************************/
 
-    if(scancode == 0x3A) { _CAPS_LOCK_STATUS = !_CAPS_LOCK_STATUS; return -0x02; }
-    if(scancode == 0x2A) { _SHIFT_STATUS = 1; return -0x02; }
+    if (scancode == 0xE0) { _MULTIMEDIA_PREFIX = 1; return -0x02; }
 
-    if(scancode >= 0x58) {
+    else if (_MULTIMEDIA_PREFIX) {
+        if (scancode == 0x49) {
+            return KEY_PAGE_UP;
+        }
+        _MULTIMEDIA_PREFIX = 0;
+    }
+
+    else if(scancode == 0x3A) { _CAPS_LOCK_STATUS = !_CAPS_LOCK_STATUS; return -0x02; }
+    else if(scancode == 0x2A) { _SHIFT_STATUS = 1; return -0x02; }
+
+    else if(scancode >= 0x58) {
         /********************/
         /* Release keys set */
         /********************/
@@ -25,39 +34,41 @@ char decode_scancode(u8 scancode)
         return -0x03;
     }
 
-    char keycode = *keycode_table[scancode];
-    if(_CAPS_LOCK_STATUS)
-    {
-        if(keycode >= 'a' && keycode <= 'z')
-            keycode = keycode - 32;
-    }
-
-    if (_SHIFT_STATUS)
-    {
-        switch (keycode) {
-            case 'a' ... 'z': keycode = keycode - 32; break;
-            case '1'  : keycode = '!'; break;
-            case '2'  : keycode = '@'; break;
-            case '3'  : keycode = '#'; break;
-            case '4'  : keycode = '$'; break;
-            case '5'  : keycode = '%'; break;
-            case '6'  : keycode = '^'; break;
-            case '7'  : keycode = '&'; break;
-            case '8'  : keycode = '*'; break;
-            case '9'  : keycode = '('; break;
-            case '0'  : keycode = ')'; break;
-            case '-'  : keycode = '_'; break;
-            case '='  : keycode = '+'; break;
-            case '['  : keycode = '{'; break;
-            case ']'  : keycode = '}'; break;
-            case '\\' : keycode = '|'; break;
-            case ';'  : keycode = ':'; break;
-            case '\'' : keycode = '"'; break;
-            case ','  : keycode = '<'; break;
-            case '.'  : keycode = '>'; break;
-            case '/'  : keycode = '?'; break;
+    else {
+        char keycode = *keycode_table[scancode];
+        if(_CAPS_LOCK_STATUS)
+        {
+            if(keycode >= 'a' && keycode <= 'z')
+                keycode = keycode - 32;
         }
-    }
 
-    return keycode;
+        if (_SHIFT_STATUS)
+        {
+            switch (keycode) {
+                case 'a' ... 'z': keycode = keycode - 32; break;
+                case '1'  : keycode = '!'; break;
+                case '2'  : keycode = '@'; break;
+                case '3'  : keycode = '#'; break;
+                case '4'  : keycode = '$'; break;
+                case '5'  : keycode = '%'; break;
+                case '6'  : keycode = '^'; break;
+                case '7'  : keycode = '&'; break;
+                case '8'  : keycode = '*'; break;
+                case '9'  : keycode = '('; break;
+                case '0'  : keycode = ')'; break;
+                case '-'  : keycode = '_'; break;
+                case '='  : keycode = '+'; break;
+                case '['  : keycode = '{'; break;
+                case ']'  : keycode = '}'; break;
+                case '\\' : keycode = '|'; break;
+                case ';'  : keycode = ':'; break;
+                case '\'' : keycode = '"'; break;
+                case ','  : keycode = '<'; break;
+                case '.'  : keycode = '>'; break;
+                case '/'  : keycode = '?'; break;
+            }
+        }
+        return keycode;
+    }
+    return -0x02;
 }
